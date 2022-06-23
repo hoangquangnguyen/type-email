6a7b204d989a088503b1ccdc861a12f79d1c682e
nhquang192 <nhquang192@gmail.com>
2022-06-23 11:40:15 +0700

    add CountDown section

---
+++ src/components/CountDown.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex h-[300px] flex-row items-center justify-around bg-[#E2F471]">
+    <div class="flex flex-col items-center gap-[22px]">
+      <h3 class="text-[44px] font-semibold leading-[55px]">1,700+</h3>
+      <p class="text-[18px] font-normal leading-[28px]">
+        Companies using Sendlance
+      </p>
+    </div>
+    <div class="flex flex-col items-center gap-[22px]">
+      <h3 class="text-[44px] font-semibold leading-[55px]">40 million+</h3>
+      <p class="text-[18px] font-normal leading-[28px]">Emails sent daily</p>
+    </div>
+    <div class="flex flex-col items-center gap-[22px]">
+      <h3 class="text-[44px] font-semibold leading-[55px]">20.000+</h3>
+      <p class="text-[18px] font-normal leading-[28px]">Users on sendlance</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "CountDown",
+});
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="overflow-x-hidden bg-[#FDF9F1]">
     <Header />
+    <CountDown />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Header from "../components/Header.vue";
+import CountDown from "@/components/CountDown.vue";
 
 export default defineComponent({
   name: "HomeView",
   components: {
     Header,
+    CountDown,
   },
 });
 </script>
